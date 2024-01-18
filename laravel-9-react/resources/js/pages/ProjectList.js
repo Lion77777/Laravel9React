f6361089726f32063678298c85be4d5eefc05241
Leon Avetisyan <l.avetisyan@andersenlab.com>
2024-01-18 14:57:08 +0400

    Initial page of data representation was edited

--- laravel-9-react/resources/js/pages/ProjectList.js
+++ laravel-9-react/resources/js/pages/ProjectList.js
@@ -1,7 +1,95 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Swal from 'sweetalert2';
+import Layout from "../components/Layout";
+import { Link } from "react-router-dom";
 
 function ProjectList() {
+    const [projectList, setProjectList] = useState([]);
 
+    useEffect(() => {
+        fetchProjectList()
+    }, []);
+
+    const fetchProjectList = () => {
+        axios.get('/api/projects')
+        .then(function (response) {
+            setProjectList(response.data);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    };
+
+    const handleDelete = (id) => {
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                axios.delete(`/api/projects/${id}`)
+                .then(function (response) {
+                    Swal.fire({
+                        icon: 'success',
+                        title: 'Project deleted successfully!',
+                        showConfirmButton: false,
+                        timer: 1500
+                    });
+                    fetchProjectList();
+                })
+                .catch(function (error) {
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'An error occured!',
+                        showConfirmButton: false,
+                        timer: 1500
+                    });
+                });
+            }
+        });
+    };
+
+    return (
+        <Layout>
+            <h2 className="text-center mt-5 mb-3">Laravel Project Manager</h2>
+            <div className="card">
+                <div className="card-header">
+                    <Link className="btn btn-outline-primary" to="/create">Create New Project</Link>
+                </div>
+                <div className="card-body">
+                    <table className="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Description</th>
+                                <th width="240px">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {projectList.map((project, key) => {
+                                return (
+                                    <tr key={key}>
+                                        <td>{project.name}</td>
+                                        <td>{project.description}</td>
+                                        <td>
+                                            <Link to={`/show/${project.id}`} className="btn btn-outline-info mx-1">Show</Link>
+                                            <Link to={`/edit/${project.id}`} className="btn btn-outline-success mx-1">Edit</Link>
+                                            <button onClick={() => handleDelete(project.id)} className="btn btn-outline-danger mx-1">Delete</button>
+                                        </td>
+                                    </tr>
+                                );
+                            })}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </Layout>
+    );
 }
 
 export default ProjectList;
